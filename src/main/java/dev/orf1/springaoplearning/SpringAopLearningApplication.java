d01797de0c6e5e150d0dcaf63357b8706b425743
Orf1 <orfeas126@gmail.com>
2022-05-27 00:25:39 -0700

    Added db support

--- src/main/java/dev/orf1/springaoplearning/SpringAopLearningApplication.java
+++ src/main/java/dev/orf1/springaoplearning/SpringAopLearningApplication.java
@@ -1,7 +1,5 @@
 package dev.orf1.springaoplearning;
 
-import dev.orf1.springaoplearning.business.Business1;
-import dev.orf1.springaoplearning.business.Business2;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,20 +11,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringAopLearningApplication implements CommandLineRunner {
     private final Logger LOGGER = LoggerFactory.getLogger(SpringAopLearningApplication.class);
 
-    Business1 business1;
-    Business2 business2;
-
     @Autowired
-    public SpringAopLearningApplication(Business1 business1, Business2 business2) {
-        this.business1 = business1;
-        this.business2 = business2;
-    }
+    public SpringAopLearningApplication() {}
 
     @Override
-    public void run(String... args) {
-        LOGGER.info(business1.calculateSomething());
-        LOGGER.info(business2.calculateSomething());
-    }
+    public void run(String... args) {}
 
     public static void main(String[] args) {
         SpringApplication.run(SpringAopLearningApplication.class, args);
