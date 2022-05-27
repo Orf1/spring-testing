d146e35973efd79a7a6e53e99d1aaf923937fa58
Orf1 <orfeas126@gmail.com>
2022-05-27 13:58:55 -0700

    update SpringAopLearningApplication.java

--- src/main/java/dev/orf1/springaoplearning/SpringAopLearningApplication.java
+++ src/main/java/dev/orf1/springaoplearning/SpringAopLearningApplication.java
@@ -1,6 +1,7 @@
 package dev.orf1.springaoplearning;
 
-import dev.orf1.springaoplearning.data.PersonJdbcDAO;
+import dev.orf1.springaoplearning.data.TagJdbcDao;
+import dev.orf1.springaoplearning.service.TestService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,18 +12,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringAopLearningApplication implements CommandLineRunner {
     private final Logger LOGGER = LoggerFactory.getLogger(SpringAopLearningApplication.class);
-    private final PersonJdbcDAO personJdbcDAO;
+    private final TagJdbcDao tagJdbcDao;
 
     @Autowired
-    public SpringAopLearningApplication(PersonJdbcDAO personJdbcDAO) {
-        this.personJdbcDAO = personJdbcDAO;
+    public SpringAopLearningApplication(TagJdbcDao tagJdbcDao) {
+        this.tagJdbcDao = tagJdbcDao;
     }
 
     @Override
     public void run(String... args) {
         LOGGER.info("Listening on {}", "http://127.0.0.1:8080");
         LOGGER.info("H2 Console running on {}", "http://127.0.0.1:8080/h2-console");
-        LOGGER.info("Find-all: {}", personJdbcDAO.findAll());
+        LOGGER.info("Find-all: {}", tagJdbcDao.findAll());
     }
 
     public static void main(String[] args) {
