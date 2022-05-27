aed842a415719c9cc9fe34ca60227bdd6e423afe
Orf1 <orfeas126@gmail.com>
2022-05-27 13:58:23 -0700

    create schema.sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,15 +1,25 @@
-create table person
+create table tag_registered
 (
-    id integer not null,
-    name varchar(255) not null,
-    location varchar(255),
-    birth_date timestamp,
-    primary key(id)
+    uuid uuid not null,
+    tag_name varchar(255) not null,
+    tag_email varchar(255),
+    tag_notes varchar(255),
+    contact_name varchar(255),
+    contact_email varchar(255),
+    contact_number varchar(255),
+    metadata varchar(255),
+    primary key (uuid)
 );
 
-INSERT INTO PERSON (ID, NAME, LOCATION, BIRTH_DATE )
-VALUES(10001, 'Ranga', 'Hyderabad', CURRENT_TIMESTAMP());
-INSERT INTO PERSON (ID, NAME, LOCATION, BIRTH_DATE )
-VALUES(10002, 'James', 'New York', CURRENT_TIMESTAMP());
-INSERT INTO PERSON (ID, NAME, LOCATION, BIRTH_DATE )
-VALUES(10003, 'Pieter', 'Amsterdam', CURRENT_TIMESTAMP());
+
+insert into tag_registered (uuid, tag_name, tag_notes, contact_name)
+values(random_uuid(), 'Harry', 'Golden Retriever', 'Orfeas M');
+insert into tag_registered (uuid, tag_name, tag_notes, contact_name)
+values(random_uuid(), 'Fluffy', 'Tibetan Mastiff', 'Shimon N');
+insert into tag_registered (uuid, tag_name, tag_notes, contact_name)
+values(random_uuid(), 'Sneaky', 'Black cat', 'John P');
+insert into tag_registered (uuid, tag_name, tag_notes, contact_name)
+values(random_uuid(), 'Rocky', 'Bearded dragon', 'Anna L');
+insert into tag_registered (uuid, tag_name, tag_notes, contact_name)
+values(random_uuid(), 'River', 'Pit-bull', 'Sienna A');
+
